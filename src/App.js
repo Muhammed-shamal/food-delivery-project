a8b5abe75087d0583caef6b65427f7d5cb2bfa5a
Ramees <rameesc985@gmail.com>
2024-11-19 11:33:22 +0530

    navbar created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
