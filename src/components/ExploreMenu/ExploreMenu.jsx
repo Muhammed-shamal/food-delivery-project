5eaf6637a6f6e33af6972d38a230526c2c033059
Ramees <rameesc985@gmail.com>
2024-11-19 14:59:24 +0530

    explore menu completed

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './ExploreMenu.css'
+import React from "react";
+import "./ExploreMenu.css";
 
-function ExploreMenu() {
+import { menu_list } from "../../assets/assets";
+
+function ExploreMenu({category, setCategory}) {
   return (
-    <div>ExploreMenu</div>
-  )
+    <div id="explore-menu" className="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring a delectable array of dishes. Our
+        mission is to satisfy your cravings and elevate your experience, one
+        delicious meal at a time.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => (
+          <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+            <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
 }
 
-export default ExploreMenu
+export default ExploreMenu;
