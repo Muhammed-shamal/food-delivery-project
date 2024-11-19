6be24bf1336de49d57794d1f094febe37b08186c
Ramees <rameesc985@gmail.com>
2024-11-19 12:16:56 +0530

    Header completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptates autem illum odio dolorem cum vero ipsam? Aliquid consectetur cumque tempore pariatur recusandae quidem quod assumenda ad libero possimus quam voluptatibus inventore similique nesciunt vel, perferendis dicta adipisci accusantium iste aspernatur autem velit repellendus nemo. Doloribus unde tempora ab dolore?</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
