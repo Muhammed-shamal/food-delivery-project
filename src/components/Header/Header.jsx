9a7995ed9e4eb2713b311884b32b81a48c7d8218
Ramees <rameesc985@gmail.com>
2024-11-20 12:32:19 +0530

    created food list component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <div className='header'>
+    <div className="header">
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptates autem illum odio dolorem cum vero ipsam? Aliquid consectetur cumque tempore pariatur recusandae quidem quod assumenda ad libero possimus quam voluptatibus inventore similique nesciunt vel, perferendis dicta adipisci accusantium iste aspernatur autem velit repellendus nemo. Doloribus unde tempora ab dolore?</p>
+        <p>
+          Choose from diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients and culinary expertise. Our
+          mission is to satisfy your cravings and elevate your dining
+          experience, one delicious meal at a time.{" "}
+        </p>
         <button>View Menu</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
