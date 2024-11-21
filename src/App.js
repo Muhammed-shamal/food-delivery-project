d0faf64ad2a69fb4693ec16e0764ac2ca52e8390
Ramees <rameesc985@gmail.com>
2024-11-21 11:54:41 +0530

    create sign in/sign up component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
@@ -5,12 +6,17 @@ import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
 function App() {
+
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
